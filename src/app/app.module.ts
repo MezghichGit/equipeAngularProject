670fd65e6f3a913cddf4680a90ba431ab1862e43
amine <amine.mezghich@ensi-uma.tn>
2021-02-24 10:33:17 +0100

    version avec composants + service + entities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddEquipeComponent } from './add-equipe/add-equipe.component';
+import { UpdateEquipeComponent } from './update-equipe/update-equipe.component';
+import { ListEquipesComponent } from './list-equipes/list-equipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEquipeComponent,
+    UpdateEquipeComponent,
+    ListEquipesComponent
   ],
   imports: [
     BrowserModule,
